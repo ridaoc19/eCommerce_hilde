fcc208fadce4b7fbb417744752c4c4f1ec47d3d5
ridaoc19 <ridaoc19@gmail.com>
2023-09-17 05:54:20 -0500

    fix(front): breadcrumb

--- client/src/features/dash/Inventory/index.tsx
+++ client/src/features/dash/Inventory/index.tsx
@@ -10,6 +10,7 @@ import Department from "./department";
 import Products from "./product/information";
 import SupplyProducts from './product/supplyProducts';
 import Subcategory from "./subCategory";
+import useProductFilter from "../../../hooks/useProductFilter";
 
 export namespace IInventory {
   export type ItemRole = {
@@ -21,7 +22,9 @@ export namespace IInventory {
 }
 
 function Inventory() {
-  const { dashboard: { state: { isLoadingProduct, breadcrumb, inventory: { department_id, category_id, subcategory_id, products_id } } } }: IContext.IContextData = useContext(CreateContext)!;
+  const { findItemById } = useProductFilter()
+  const { dashboard: { state: { isLoadingProduct, inventory } } }: IContext.IContextData = useContext(CreateContext)!;
+  const { department_id, category_id, subcategory_id, products_id } = inventory;
   const queryClient = useQueryClient();
   const data: MakeProductsRequestReturn | undefined = queryClient.getQueryData(IProduct.PRODUCT_NAME_QUERY);
   const isLoading = queryClient.isFetching(IProduct.PRODUCT_NAME_QUERY);
@@ -32,10 +35,12 @@ function Inventory() {
   const product = data?.products?.find(dep => dep._id === department_id)?.categoriesId.find(cat => cat._id === category_id)?.subcategoriesId.find(sub => sub._id === subcategory_id)?.productsId;
   const supplyProducts = data?.products?.find(dep => dep._id === department_id)?.categoriesId.find(cat => cat._id === category_id)?.subcategoriesId.find(sub => sub._id === subcategory_id)?.productsId.find(pro => pro._id === products_id);
 
+  const ID = Object.entries(inventory)?.filter(([_key, value]) => value)?.at(-1)
+
   return (
     <>
       {!!isLoading && !isLoadingProduct && <div>Actualizando Productos...</div>}
-      <h4>{breadcrumb}</h4>
+      {ID && <h4>{findItemById({ id: ID[1] ? ID[1] : '' }).breadcrumb}</h4>}
       {isLoadingProduct
         ? <div>Cargando Productos...</div>
         : <div>
@@ -44,19 +49,18 @@ function Inventory() {
               <h2>Departamento</h2>
               {<Department department={department} />}
             </div>
-            <div className="department">
+            <div className="category">
               <h2>Categoría</h2>
               {department_id && category && <Category category={category} />}
-              {/* {department_id && category && <Category category={category} />} */}
             </div>
-            <div className="department">
+            <div className="subcategory">
               <h2>Subcategoría</h2>
               {category_id && subcategory && <Subcategory subcategory={subcategory} />}
             </div>
           </div>}
 
           {<div className="admin_inventory">
-            <div className="department">
+            <div className="products">
               <h2>Products</h2>
               {subcategory_id && product && <Products products={product} />}
               {products_id && supplyProducts && <SupplyProducts product={supplyProducts} />}
