0e064f6e2e984b231704eab16a52915dfa61e318
ridaoc19 <ridaoc19@gmail.com>
2024-01-17 04:13:57 -0500

    style card

--- client/src/pages/navigation/listProducts/ListProducts.tsx
+++ client/src/pages/navigation/listProducts/ListProducts.tsx
@@ -1,9 +1,11 @@
 
 import Card from "../../../components/common/card/Card";
 import useListProduct from "../../../hooks/useListProduct/useListProduct";
+import useMediaQuery from "../../../hooks/useMediaQuery";
 
 function ListProducts() {
-  const { Filters, BreadcrumbComponent, listProducts, PaginationButton, currentIndex, paginationTotal, totalProduct } = useListProduct()
+  const { mediaQuery } = useMediaQuery()
+  const { BreadcrumbComponent, listProducts, currentIndex, paginationTotal, totalProduct } = useListProduct()
 
   return (
     <div className="list-product__container">
@@ -13,11 +15,21 @@ function ListProducts() {
         - posicion:{currentIndex}
         - paginacion Total: {paginationTotal}
         - Total Productos: {totalProduct}
-        {PaginationButton}
+        {/* {PaginationButton} */}
         {/* <Breadcrumb /> */}
       </div>
-      <div className="list-product__card" style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', gap: '1rem' }}>
-        {Filters}
+      <div className="list-product__card"
+        style={{
+          // display: 'flex', 
+          // flexWrap: 'wrap',
+          display: 'grid',
+          gridTemplateColumns: `repeat(auto-fit, minmax(9rem, ${mediaQuery === 'phone' ? '9rem' : mediaQuery === 'tablet' ? '15rem' : '20rem'}))`,
+          justifyContent: 'space-evenly',
+          //  justifyContent: 'space-between',
+          gap: '0.5rem'
+        }}
+      >
+        {/* {Filters} */}
         {listProducts.map(({ product: { product_id, product, brand }, variants }) => {
           return (
             <Card key={product_id} product_id={product_id} product={product} brand={brand} images={variants[0].images} price={variants.map(variant => variant.price)} />
