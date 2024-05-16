e746e5aa833aa316cdb55d04ff048ae3dd0e3a6e
ricardo <ridaoc19@gmail.com>
2024-05-16 15:24:38 +0200

    creation nestjs

--- api/dotenv.ts
+++
@@ -1,4 +0,0 @@
-import 'dotenv/config';
-import * as dotenv from 'dotenv'
-// dotenv.config({ path: `.env.${process.env.NODE_ENV}` })
-dotenv.config({ path: process.env.NODE_ENV === 'development' ? '.env.development' : '.env.production' })
--- api/src/core/utils/findParentUUID.ts
+++
@@ -1,5 +0,0 @@
-
-export function findParentUUID(data: string): boolean {
-  const regexUUID = /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/;
-  return regexUUID.test(data)
-}
